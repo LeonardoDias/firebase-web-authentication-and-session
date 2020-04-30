ca91c9640494ab1cf8d1e47db373342b2d74d1c5
Leonardo Dias <leonardodias914@gmail.com>
2020-04-30 11:09:56 -0300

    Add app route

---
+++ src/routes/app/index.route.ts
@@ -0,0 +1,75 @@
+import { Router, response } from "express"
+import path from 'path'
+import passport from "passport";
+import request from "request-promise"
+
+const router = Router();
+
+router.get('/login', (req, res, next) => {
+    passport.authenticate('jwt', 
+        {
+            session: false, 
+            successRedirect: '/home',
+        },
+        (err, user) => {
+            if(err) {
+                next(err)
+                return
+            } else if(!user) {
+                res.sendFile(path.join(process.env.ROOT+'/pages/index.html'))
+            } else {
+                res.redirect('/home')
+            }
+         }
+    )(req, res, next)
+})
+
+router.post('/login', (req, res, next) => {
+    passport.authenticate('jwt', 
+        {
+            session: false, 
+            successRedirect: '/home',
+    }, (err, user) => {
+        if(err) {
+            next(err)
+            return
+        }
+        request.post('http://localhost:3000/api/auth', {
+           body: req.body,
+           json: true,
+           resolveWithFullResponse: true
+        }).then((data) => {
+            res.cookie('sid', data.body.key, {
+                expires: new Date(Date.now() +  3600000),
+                httpOnly: true,
+                secure: false
+            })
+            res.redirect('/home')
+        }).catch((err) => {
+            if(err.statusCode === 401 || err.statusCode === 400) {
+                res.status(err.statusCode).sendFile(path.join(process.env.ROOT+'/pages/index.html'))
+                return
+            }
+            next(err)
+        })
+    })(req, res, next)
+})
+
+router.post('/logout', (req, res, next) => {
+    passport.authenticate('jwt', 
+        {
+            session: false,
+            failureRedirect: '/'
+        },
+        (err, user) => {
+            if(err) {
+                next(err)
+                return
+            } else {
+                res.clearCookie('sid')
+                res.redirect(301, '/')
+            }
+         }
+    )(req, res, next)
+})
+export default router
---
+++ src/routes/app/index.ts
@@ -0,0 +1,6 @@
+
+import app from './index.route'
+
+export default {
+    app
+}
